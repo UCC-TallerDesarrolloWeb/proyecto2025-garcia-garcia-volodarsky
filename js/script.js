f1b05ea78c20817af3fdd3381b570ae7a59a96af
JuanGarcia133 <2411121@ucc.edu.ar>
2025-09-29 11:15:54 -0300

    home y roster sumadas, faltan info en cad una

--- js/script.js
+++ js/script.js
@@ -2,14 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Página lista ✅");
 });
 
-// Scroll suave para navegación interna
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener("click", e => {
-    const id = a.getAttribute("href");
-    const el = document.querySelector(id);
-    if (el) {
-      e.preventDefault();
-      el.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener("click", e=>{
+    const id=a.getAttribute("href");
+    const el=document.querySelector(id);
+    if(el){ e.preventDefault(); el.scrollIntoView({behavior:"smooth",block:"start"}); }
   });
 });
